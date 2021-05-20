185979f19b93afd11a12d979b1efdff8c4dded40
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-05-20 12:38:49 -0300

    Adding the model with factory and seeder for the Frequencies entity

---
+++ database/migrations/2021_05_20_153148_create_frequencies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFrequenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('frequencies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('date');
+            $table->boolean('presence');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('frequencies');
+    }
+}
