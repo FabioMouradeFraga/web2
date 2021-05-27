56c67cc008fbe85a2efa47613131b0c315daa725
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-05-26 21:46:02 -0300

    Listing students that belong to the user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    Seja bem-vindo! Aqui estão os seus alunos:
                 </div>
+                    <table class="table-auto">
+                      <thead>
+                        <tr>
+                          <th>Nome</th>
+                          <th>E-mail</th>
+                          <th>Turma</th>
+                        </tr>
+                      </thead>
+                    @foreach (Auth::user()->students as $students)
+                      <tbody>
+                        <tr>
+                          <td class="px-2">{{ $students->name }}</td>
+                          <td class="px-2">{{ $students->email }}</td>
+                          <td class="px-2">{{ $students->grades->name }}</td>
+                        </tr>
+                    @endforeach
             </div>
         </div>
     </div>
